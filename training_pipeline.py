7d4e6f4a42b1f6f1881fd6d87621c7abee3a3bcc
vincenzocivale <civalevincenzoyuto@gmail.com>
2025-08-05 02:21:36 +0200

    Enhance training pipeline with class balancing and increased epochs
    - Adjusted training batch size from 8 to 32 and increased number of epochs from 70 to 7000 for improved training duration.
    - Implemented undersampling technique to balance class distribution in the dataset.
    - Modified dataset splitting to ensure stratified sampling for training, validation, and test sets.
    - Updated plotting function to visualize class distributions after undersampling.
    - Changed evaluation and logging parameters in TrainingArguments for better monitoring.
    - Increased early stopping patience from 3 to 10 evaluations to allow for more training iterations.

--- training_pipeline.py
+++ training_pipeline.py
@@ -5,9 +5,10 @@ import torchvision.transforms as transforms
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Subset, DataLoader
 from transformers import get_linear_schedule_with_warmup, get_cosine_schedule_with_warmup
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-# from src.utils.hf_utils import download_weights
-# from src.utils.vit_config import inizialize_model
 from src.data.dataset import PatchFromH5Dataset, stratified_split, plot_class_distributions
 from src.rl.train import ModelTrainer, TrainingArguments
 
@@ -16,8 +17,8 @@ from src.rl.modelling import ViT_UCB_Pruning
 
 # %%
 IMG_SIZE = 224
-TRAIN_BATCH_SIZE = 8
-NUM_EPOCHS = 70
+TRAIN_BATCH_SIZE = 32
+NUM_EPOCHS = 7000
 
 # %%
 dataset = PatchFromH5Dataset(
@@ -28,15 +29,56 @@ dataset = PatchFromH5Dataset(
 )
 
 # %%
-train_idx, val_idx, test_idx = stratified_split(dataset, test_size=0.3, val_size=0.3, random_state=42)
+labels = dataset.labels
 
 # %%
-plot_class_distributions(train_idx, val_idx, test_idx, full_dataset=dataset)
+# Crea un DataFrame con indici e label
+df = pd.DataFrame({
+    "index": np.arange(len(labels)),
+    "label": labels
+})
+
+# Trova il numero di elementi della classe minoritaria
+min_count = df["label"].value_counts().min()
+
+# Per ogni classe, seleziona min_count elementi a caso
+undersampled_df = (
+    df.groupby("label", group_keys=False)
+      .apply(lambda x: x.sample(n=min_count, random_state=42)).reset_index(drop=True)
+)
+
+# Mischia gli indici
+undersampled_indices = undersampled_df["index"].sample(frac=1, random_state=42).tolist()
+
 
 # %%
+undersampled_labels = [labels[i] for i in undersampled_indices]
+
+trainval_idx, test_idx = train_test_split(
+    undersampled_indices,
+    test_size=0.3,
+    stratify=undersampled_labels,
+    random_state=42
+)
+
+# Ottieni i label corrispondenti per il secondo split
+trainval_labels = [labels[i] for i in trainval_idx]
+
+# Split: train vs val
+train_idx, val_idx = train_test_split(
+    trainval_idx,
+    test_size=0.3,
+    stratify=trainval_labels,
+    random_state=42
+)
+
+# Crea i subset
 train_dataset = Subset(dataset, train_idx)
-val_dataset = Subset(dataset, val_idx)
-test_dataset = Subset(dataset, test_idx)
+val_dataset   = Subset(dataset, val_idx)
+test_dataset  = Subset(dataset, test_idx)
+
+# %%
+plot_class_distributions(train_dataset, val_dataset, test_dataset, full_dataset=dataset)
 
 # %%
 train_loader = DataLoader(train_dataset, batch_size=TRAIN_BATCH_SIZE, shuffle=True, num_workers=16, drop_last=True)
@@ -44,7 +86,7 @@ val_loader = DataLoader(val_dataset, batch_size=TRAIN_BATCH_SIZE, shuffle=False,
 test_loader = DataLoader(test_dataset, batch_size=TRAIN_BATCH_SIZE, shuffle=False, num_workers=16, drop_last=True)
 
 # %%
-labels_num = len(dataset.labels)
+labels_num = len(np.unique(dataset.labels))
 
 print(f"Number of classes: {labels_num}")
 model = ViT_UCB_Pruning(model_name="hf-hub:MahmoodLab/uni", pretrained=True, n_classes=labels_num)
@@ -52,22 +94,21 @@ model = ViT_UCB_Pruning(model_name="hf-hub:MahmoodLab/uni", pretrained=True, n_c
 # %%
 args = TrainingArguments(
         output_dir="./results",
-        run_name="ViT-L-UCB-Pruning-run1",
+        run_name="ViT-L-UCB-Pruning-MultiClass",
         num_train_epochs=NUM_EPOCHS,
         learning_rate=0.1,
         train_batch_size=8,
         eval_batch_size=8,
         max_steps=20000,
         warmup_steps=500,
-        eval_steps=92406,
-        save_steps=92406,
-        logging_steps=2500,
+        eval_steps=5000,
+        save_steps=10000,
+        logging_steps=50,
         fp16=False,
         report_to="wandb", 
-        early_stopping_patience=3, # Interrompi dopo 3 valutazioni senza miglioramento
+        early_stopping_patience=10, # Interrompi dopo 3 valutazioni senza miglioramento
         early_stopping_metric="eval/loss", # Oppure monitora la loss (un valore più basso è meglio)
     )
-
 
 
 # %%
@@ -77,7 +118,6 @@ num_steps = args.num_train_epochs * (len(train_loader) // args.gradient_accumula
 scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=num_steps)
 
 # %%
-
 trainer = ModelTrainer(
         model=model,
         args=args,
